ac4fc3b52fd2104edbb8f22877c01c9dfb231bb1
Philip Schmidt <philip.schmidt@networkteam.com>
2022-05-01 22:55:31 +0200

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,42 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import scss from 'rollup-plugin-scss';
+import visualizer from 'rollup-plugin-visualizer';
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
 
-const packageJson = require('./package.json');
-
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      scss({
-        output: 'dist/css/react-mirt.css',
-        watch: 'src/scss',
-        failOnError: true,
-        outputStyle: 'compressed',
-        runtime: require('sass'),
-        processor: () => postcss([autoprefixer()]),
-      }),
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      terser(),
-    ],
-    external: ['react', 'react-dom'],
+export default {
+  input: ['./src/index.ts'],
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+    sourcemap: true,
   },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    external: [/\.scss$/],
-    plugins: [dts()],
-  },
-];
+  plugins: [
+    scss({
+      output: 'dist/css/react-mirt.css',
+      watch: 'src/scss',
+      failOnError: true,
+      outputStyle: 'compressed',
+      runtime: require('sass'),
+      processor: () => postcss([autoprefixer()]),
+    }),
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+    }),
+    terser(),
+    visualizer({
+      filename: 'bundle-analysis.html',
+      open: true,
+    }),
+  ],
+  external: ['react', 'react-dom'],
+};
